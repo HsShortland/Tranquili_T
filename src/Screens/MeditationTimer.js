a257fed84dcb99d2fad8711600bfbd7593ea7259
Sydney Humayun <psychdreamer@hotmail.co.uk>
2022-12-10 13:11:14 +0000

    installed background timer package from react and imported useEffect and UseState

--- src/Screens/MeditationTimer.js
+++ src/Screens/MeditationTimer.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, Button, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useDrawerStatus } from '@react-navigation/drawer';
 import styles from '../components/ReusableStyleSheet';
+import BackgroundTimer from 'react-native-background-timer';
+import React, {useEffect, useState} from 'react';
 
 export default function MeditationTimer() {
     const nav = useNavigation();
@@ -11,7 +13,17 @@ export default function MeditationTimer() {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Meditation Timer</Text>
+            <Text style={SVGAnimatedLengthList.time}>Hello</Text>
             <StatusBar style="auto" />
     </View>
 );
-}
+};
+
+
+const styles = StyleSheet.create({
+    time: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 30,
+    },
+})
