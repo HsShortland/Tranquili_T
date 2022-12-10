53958964a824eed8e3d3c15debbb037fb040acca
Sydney Humayun <psychdreamer@hotmail.co.uk>
2022-12-10 13:30:39 +0000

    added UseEffect logic

--- src/Screens/MeditationTimer.js
+++ src/Screens/MeditationTimer.js
@@ -10,6 +10,19 @@ export default function MeditationTimer() {
     const nav = useNavigation();
     const drawerStatus = useDrawerStatus();
     console.log(drawerStatus);
+
+    const [secondsLeft, setSecondsLeft] = useState(3601);
+    const [timerOn, setTimerOn] = useState(false);
+
+    useEffect(() => {
+        if (timerOn) startTimer();
+        else BackgroundTimer.stopBackgroundTimer();
+    
+    return () => {
+        BackgroundTimer.stopBackgroundTimer();
+    };
+    }, [timerOn]);
+
     return (
         <View style={styles.container}>
             <Text style={styles.time}>Hello</Text>
