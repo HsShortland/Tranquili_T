f655b13bd34f806ce0b32b63ff8acefa251e9be1
Sydney Humayun <psychdreamer@hotmail.co.uk>
2022-12-11 20:54:13 +0000

    removed extraneous code and code that didn't make sense

--- src/Screens/MeditationTimer.js
+++ src/Screens/MeditationTimer.js
@@ -4,77 +4,62 @@ import { useNavigation } from '@react-navigation/native';
 import { useDrawerStatus } from '@react-navigation/drawer';
 import styles from '../components/ReusableStyleSheet';
 import React, {useEffect, useState} from 'react';
-import Timer from '../components/timer'
+import StartButton from '../components/StartButton';
+import ResetButton from '../components/ResetButton';
 
 
 
-export default function MeditationTimer() {
+
+export default function MeditationTimer({ timeForSeconds }) {
     const nav = useNavigation();
     const drawerStatus = useDrawerStatus();
     console.log(drawerStatus);
 
     const [timerClicked, setTimerClicked] = useState(false);
     const [time, setTime] = useState(600);
-    const [timer, setTimer] = useState(0);
-    const [timerOn, setTimerOn] = useState(false);
-    const [displayTimer, setDisplayTimer] = useState(timer);
+    const [displayTimer, setDisplayTimer] = useState(time);
+    const [secondsLeft, setSecondsLeft] = useState(timeForSeconds);
+    
    
-
-    const onPressTimerItem = (time) => {
-        setTimerClicked((prevState) => !prevState);
-        setTimer(time);
-      };
 
 
     const startTimer = () => {
         setTimerClicked((prevState) => !prevState)
-        setInterval(() => {
-            setTime((prevTime) => prevTime - 1)
-        }, 1000)
     };
 
+    const resetTimer = () => {
+        
+    }
 
     useEffect(() => {
-        if (timerOn)  startTimer();{
-          return;
-        }
-        setDisplayTimer(timer);
-    });
+        const interval = setInterval(() => {
+            if(secondsLeft > 0) setSecondsLeft(secondsLeft - 1);
+        }, 1000);
+        return () => clearInterval(interval);
+        });
+
+    // useEffect(() => {
+    //     if (timerOn)  startTimer();{
+    //       return;
+    //     }
+    //     setDisplayTimer(timer);
+    // });
     
-    //     const interval = setInterval(() => {
-    //       setDisplayTimer(() =>
-    //         prevTimer > 0 ? prevTimer - 1 : clearInterval(interval)
-    //       );
-    //     }, 1000);
-    //   }, [timerOn, setTimerOn, timer]);
 
     
 
 
 return (
    <View style={styles.container}>
-     {timerClicked && <Timer time={time} />}
-     <Text style={styles.displayTimerText}>{displayTimer}</Text>
+     
+     <Text style={styles.displayTimerText}>{secondsLeft}</Text>
+     <StartButton details= "Start/Stop"></StartButton>
+     <ResetButton details= "Reset"></ResetButton>
      <View style={styles.timerContainer}>
-        <Text></Text>
-        
-            <Button
-                title="Start/Stop"
-                onPress={ startTimer }
-                color="gray"
-            />
-
-            <Button
-                title="Reset"
-                // onPress={}
-                color="gray"
-            />
             
         </View>
     </View>
-    //         {timerOn && (
-             
-    //         )}
+
      );
 };
 
