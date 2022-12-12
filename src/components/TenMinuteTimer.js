cea303cc38b419918214757bb8cac58e156bff72
Sydney Humayun <psychdreamer@hotmail.co.uk>
2022-12-12 20:38:48 +0000

    created 10 minute tiimer button component

---
+++ src/components/TenMinuteTimer.js
@@ -0,0 +1,41 @@
+
+import { Pressable, StyleSheet, Text } from "react-native";
+import React, {useEffect, useState} from 'react';
+import styles from './ReusableStyleSheet';
+
+export default function TenMinute() {
+
+     // time
+    const [secondsLeft, setSecondsLeft] = useState(600);
+    
+
+    function tenStart() {
+        useEffect(() => {
+            const interval = setInterval(() => {
+                if(secondsLeft > 0) setSecondsLeft(secondsLeft - 1);
+            }, 1000);
+            return () => clearInterval(interval);
+            });
+        };
+
+
+    const clockIt = () => {
+        let mins = Math.floor((secondsLeft / 60) % 60);
+        let seconds = Math.floor(secondsLeft % 60);
+
+        let displayMins = mins < 10 ? `0${mins}` : mins;
+        let displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+        return {
+            displayMins: displayMins,
+            displaySeconds: displaySeconds,
+        };
+    };
+
+    return(
+        <Pressable style={styles.greyBlueButtonStyle} onPress={tenStart}>
+            <Text>{clockIt().displayMins} Mins {clockIt().displaySeconds} Secs </Text>
+        </Pressable>
+    );
+}
+
+
